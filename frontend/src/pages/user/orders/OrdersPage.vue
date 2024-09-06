cf2f76790f419601b8c5b4f95ec41d135de24b66
syy0O <songie05@naver.com>
2024-09-06 16:19:32 +0900

    [Feat] OrdersPages 구현 (#92)

---
+++ frontend/src/pages/user/orders/OrdersPage.vue
@@ -0,0 +1,18 @@
+<template>
+  <OrdersMainComponent />
+</template>
+
+<script>
+import OrdersMainComponent from "@/components/orders/OrdersMainComponent.vue";
+export default {
+  name: "OrdersPage",
+  props: {
+    msg: String,
+  },
+  components: {
+    OrdersMainComponent,
+  },
+};
+</script>
+<style scoped>
+</style>
