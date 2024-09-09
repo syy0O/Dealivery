21ad25744b53f0110afa1d86c44fe386067abcd6
kangkings <69503955+kangkings@users.noreply.github.com>
2024-09-09 09:27:52 +0900

    [Feat] 비밀번호찾기 대기 화면 구현 (#105) (#109)

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -7,6 +7,7 @@ import CompanyBoardPostPage from "../pages/company/board/CompanyBoardPostPage.vu
 import OrdersPage from "@/pages/user/orders/OrdersPage.vue";
 import { createRouter, createWebHistory } from "vue-router";
 import CompanySignupComponent from "@/components/company/CompanySignupComponent.vue";
+import FindPasswordWaitComponent from "@/components/user/FindPasswordWaitComponent.vue";
 import FindPasswordComponent from "@/components/user/FindPasswordComponent.vue";
 import ResetPasswordComponent from "@/components/user/ResetPasswordComponent.vue";
 import FindIdSuccessComponent from "@/components/user/FindIdSuccessComponent.vue";
@@ -35,6 +36,7 @@ const router = createRouter({
         {path: "login", component: LoginComponent, meta: { requiresAuth: false } },
         {path: "user/signup", component: UserSignupComponent, meta: { requiresAuth: false } },
         {path: "company/signup", component: CompanySignupComponent, meta: { requiresAuth: false } },
+        {path: "pwd/find/wait", component: FindPasswordWaitComponent, meta: { requiresAuth: false } },
         {path: "pwd/find", component: FindPasswordComponent, meta: { requiresAuth: false } },
         {path: "pwd/reset", component: ResetPasswordComponent, meta: { requiresAuth: false } },
         {path: "id/find/success", component: FindIdSuccessComponent, meta: { requiresAuth: false } },
