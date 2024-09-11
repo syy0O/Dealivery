52b54eac7ac9b58840176ddab962aa8ad12b78e1
nado <101664417+NakyungSong@users.noreply.github.com>
2024-09-11 17:08:34 +0900

    [#85] 문의 등록 화면 구현 (#133)
    * [Feat] 문의 등록 모달창 컴포넌트 구현 (#85)
    
    * [Feat] 문의하기 버튼 클릭 시 모달 창이 나타나도록 구현 (#85)
    
    ---------
    
    Co-authored-by: NakyungSong <thdsk4311@sju.ac.krgit config --global user.email thdsk4311@sju.ac.krgit config --list>

--- frontend/src/components/board/BoardDetailNavComponent.vue
+++ frontend/src/components/board/BoardDetailNavComponent.vue
@@ -2,37 +2,27 @@
   <div class="css-16c0d8l">
     <nav class="css-1le17tz en4zazl1">
       <ul class="css-tse2s2 en4zazl0">
-        <li
-          :class="[
-            'css-1tzhzcg',
-            'efe6b6j1',
-            'tab',
-            { active: activeTab === 'description' },
-          ]"
-          @click.prevent="activeTab = 'description'"
-        >
+        <li :class="[
+          'css-1tzhzcg',
+          'efe6b6j1',
+          'tab',
+          { active: activeTab === 'description' },
+        ]" @click.prevent="activeTab = 'description'">
           <a class="css-1t0ft7s efe6b6j0"><span class="name">상품설명</span></a>
         </li>
-        <li
-          :class="[
-            'css-1tzhzcg',
-            'efe6b6j1',
-            'tab',
-            { active: activeTab === 'inquiries' },
-          ]"
-          @click.prevent="activeTab = 'inquiries'"
-        >
+        <li :class="[
+          'css-1tzhzcg',
+          'efe6b6j1',
+          'tab',
+          { active: activeTab === 'inquiries' },
+        ]" @click.prevent="activeTab = 'inquiries'">
           <a class="css-1t0ft7s efe6b6j0"><span class="name">문의</span></a>
         </li>
       </ul>
     </nav>
     <!--상품 상세 설명 section-->
     <div class="css-0 el27cq1">
-      <div
-        id="description"
-        class="css-18eozqj el27cq0"
-        v-show="activeTab === 'description'"
-      >
+      <div id="description" class="css-18eozqj el27cq0" v-show="activeTab === 'description'">
         <div class="css-1d3w5wq e1d86arr0">
           <div class="css-1lyi66c">
             <div class="goods_wrap">
@@ -40,8 +30,7 @@
                 <div class="context">
                   <div class="pic">
                     <img
-                      src="https://img-cf.kurly.com/hdims/resize/%3E1010x/quality/90/src/shop/data/goodsview/20240829/gv10001551896_1.jpg"
-                    />
+                      src="https://img-cf.kurly.com/hdims/resize/%3E1010x/quality/90/src/shop/data/goodsview/20240829/gv10001551896_1.jpg" />
                   </div>
                   <p class="words"></p>
                 </div>
@@ -56,12 +45,7 @@
     <!--문의 하기 리스트 section-->
     <div class="css-30tvht eewa3w91" v-show="activeTab === 'inquiries'">
       <div class="css-17juoyc eewa3w90">
-        <button
-          class="css-mhrz8m e4nu7ef3"
-          type="button"
-          width="120"
-          height="40"
-        >
+        <button class="css-mhrz8m e4nu7ef3" type="button" width="120" height="40" @click="openModal">
           <span class="css-nytqmg e4nu7ef1">문의하기</span>
         </button>
       </div>
@@ -85,11 +69,7 @@
             </tr>
           </thead>
           <tbody>
-            <tr
-              v-for="(row, index) in tableData"
-              :key="index"
-              class="css-atz965 e1l5ky7y9"
-            >
+            <tr v-for="(row, index) in tableData" :key="index" class="css-atz965 e1l5ky7y9">
               <td class="css-1brd6ns e1l5ky7y8">{{ row.title }}</td>
               <td class="css-1pkqelu e1l5ky7y7">{{ row.author }}</td>
               <td class="css-1pkqelu e1l5ky7y6">{{ row.createdDate }}</td>
@@ -100,26 +80,21 @@
       </div>
       <div class="css-18ad0gx e9e6ap50">
         <div class="css-sxxs1g eytury60">
-          <button
-            disabled="!hasPrev"
-            type="button"
-            class="css-rzcdhr e1hbwyso0"
-          >
-            <div class="css-7qb0sc e1ilyb3p0">이전</div></button
-          ><button
-            disabled="!hasNext"
-            type="button"
-            class="css-1jwilit e1pk9060"
-          >
+          <button :disabled="!hasPrev" type="button" class="css-rzcdhr e1hbwyso0">
+            <div class="css-7qb0sc e1ilyb3p0">이전</div>
+          </button><button :disabled="!hasNext" type="button" class="css-1jwilit e1pk9060">
             <div class="css-7qb0sc e1ilyb3p0">다음</div>
           </button>
         </div>
       </div>
     </div>
+    <QnaRegisterModalComponent v-if="showModal" @close="closeModal" />
   </div>
 </template>
 
 <script>
+import QnaRegisterModalComponent from '../qna/QnaRegisterModalComponent.vue';
+
 export default {
   name: "BoardDetailNavComponent",
   props: {
@@ -134,10 +109,23 @@ export default {
       activeTab: "description",
       hasPrev: false,
       hasNext: true,
+      showModal: false, // 모달을 관리할 변수 추가
     };
+  },
+  methods: {
+    openModal() {
+      this.showModal = true; // 모달 열기
+    },
+    closeModal() {
+      this.showModal = false; // 모달 닫기
+    },
+  },
+  components: {
+    QnaRegisterModalComponent,
   },
 };
 </script>
+
 <style scoped>
 button {
   cursor: pointer;
@@ -313,8 +301,7 @@ table {
   display: block;
   width: 14px;
   height: 14px;
-  background: url(data:image/svg+xml;base64,PHN2ZyB3aWR0aD0iMTIiIGhlaWdodD0iMTQiIHZpZXdCb3g9IjAgMCAxMiAxNCIgeG1sbnM9Imh0dHA6Ly93d3cudzMub3JnLzIwMDAvc3ZnIj4KICAgIDxnIGZpbGw9IiNCNUI1QjUiIGZpbGwtcnVsZT0iZXZlbm9kZCI+CiAgICAgICAgPHBhdGggZD0iTTExLjA0MyAxMi41NzVhLjI1OS4yNTkgMCAwIDEtLjI1OC4yNTdIMS4yMTRhLjI1OS4yNTkgMCAwIDEtLjI1OC0uMjU3VjUuNDJjMC0uMTQyLjExNy0uMjU4LjI1OC0uMjU4aDkuNTdjLjE0NCAwIC4yNi4xMTYuMjYuMjU4djcuMTU1ek0zLjY4NSAzLjIzN0EyLjI4MyAyLjI4MyAwIDAgMSA1Ljk2Ni45NTdhMi4yODIgMi4yODIgMCAwIDEgMi4yODEgMi4yOHYuOTY4SDMuNjg1di0uOTY4em03LjEuOTY4aC0xLjU4di0uOTY4QTMuMjQxIDMuMjQxIDAgMCAwIDUuOTY1IDAgMy4yNCAzLjI0IDAgMCAwIDIuNzMgMy4yMzd2Ljk2OEgxLjIxNEMuNTQ0IDQuMjA1IDAgNC43NSAwIDUuNDJ2Ny4xNTVjMCAuNjY5LjU0NSAxLjIxNCAxLjIxNCAxLjIxNGg5LjU3Yy42NzEgMCAxLjIxNi0uNTQ1IDEuMjE2LTEuMjE0VjUuNDJjMC0uNjctLjU0NS0xLjIxNS0xLjIxNS0xLjIxNXoiLz4KICAgICAgICA8cGF0aCBkPSJNNSA4LjJjMCAuMzQzLjE4NC42MzIuNDQ4LjgxMnYxLjMzaDEuMTAzdi0xLjMzQS45ODYuOTg2IDAgMCAwIDcgOC4yYTEgMSAwIDAgMC0yIDB6Ii8+CiAgICA8L2c+Cjwvc3ZnPgo=)
-    no-repeat;
+  background: url(data:image/svg+xml;base64,PHN2ZyB3aWR0aD0iMTIiIGhlaWdodD0iMTQiIHZpZXdCb3g9IjAgMCAxMiAxNCIgeG1sbnM9Imh0dHA6Ly93d3cudzMub3JnLzIwMDAvc3ZnIj4KICAgIDxnIGZpbGw9IiNCNUI1QjUiIGZpbGwtcnVsZT0iZXZlbm9kZCI+CiAgICAgICAgPHBhdGggZD0iTTExLjA0MyAxMi41NzVhLjI1OS4yNTkgMCAwIDEtLjI1OC4yNTdIMS4yMTRhLjI1OS4yNTkgMCAwIDEtLjI1OC0uMjU3VjUuNDJjMC0uMTQyLjExNy0uMjU4LjI1OC0uMjU4aDkuNTdjLjE0NCAwIC4yNi4xMTYuMjYuMjU4djcuMTU1ek0zLjY4NSAzLjIzN0EyLjI4MyAyLjI4MyAwIDAgMSA1Ljk2Ni45NTdhMi4yODIgMi4yODIgMCAwIDEgMi4yODEgMi4yOHYuOTY4SDMuNjg1di0uOTY4em03LjEuOTY4aC0xLjU4di0uOTY4QTMuMjQxIDMuMjQxIDAgMCAwIDUuOTY1IDAgMy4yNCAzLjI0IDAgMCAwIDIuNzMgMy4yMzd2Ljk2OEgxLjIxNEMuNTQ0IDQuMjA1IDAgNC43NSAwIDUuNDJ2Ny4xNTVjMCAuNjY5LjU0NSAxLjIxNCAxLjIxNCAxLjIxNGg5LjU3Yy42NzEgMCAxLjIxNi0uNTQ1IDEuMjE2LTEuMjE0VjUuNDJjMC0uNjctLjU0NS0xLjIxNS0xLjIxNS0xLjIxNXoiLz4KICAgICAgICA8cGF0aCBkPSJNNSA4LjJjMCAuMzQzLjE4NC42MzIuNDQ4LjgxMnYxLjMzaDEuMTAzdi0xLjMzQS45ODYuOTg2IDAgMCAwIDcgOC4yYTEgMSAwIDAgMC0yIDB6Ii8+CiAgICA8L2c+Cjwvc3ZnPgo=) no-repeat;
   margin-left: 6px;
 }
 
@@ -371,7 +358,7 @@ table {
   cursor: default;
 }
 
-.css-sxxs1g button + button {
+.css-sxxs1g button+button {
   margin-left: 12px;
 }
 
@@ -402,10 +389,12 @@ table {
 }
 
 .tab.active {
-  background: #fff; /* 활성화된 탭 배경색 */
+  background: #fff;
+  /* 활성화된 탭 배경색 */
 }
 
 .tab.active a span {
-  color: #5f0080; /* 활성화된 탭 텍스트 색상 */
+  color: #5f0080;
+  /* 활성화된 탭 텍스트 색상 */
 }
 </style>
