0eed2dbd52e2f8d5132e9785c6363758e8e0d95d
GyuLim Oh <77095330+ohgyulim@users.noreply.github.com>
2024-09-14 10:48:00 +0900

    [#33] 상품 게시글 목록 조회 (업체회원) 기능 구현 (#151)
    * [Feat] 업체회원 이메일 인증 기능 구현 (#17)
    
    * [Fix] 업체회원 이메일 인증 기능 수정 - 요청URI 소문자로 변경 (#17)
    
    * [Feat] 업체회원가입 기능 구현 (#16)
    
    * [Feat] 상품 게시글 목록 조회 (업체회원) 기능 구현 (#33)
    
    * [Feat] 스웨거 등록 (#33)
    
    * [Feat] 상품 게시글 목록 조회 (업체회원) 기능 구현 (#33)
    
    ---------
    
    Co-authored-by: kangkigs <dlflq11@gmail.com>

--- backend/src/main/java/org/example/backend/domain/board/controller/BoardController.java
+++ backend/src/main/java/org/example/backend/domain/board/controller/BoardController.java
@@ -3,25 +3,42 @@ package org.example.backend.domain.board.controller;
 import org.example.backend.domain.board.model.dto.BoardDto;
 import org.example.backend.domain.board.service.ProductBoardService;
 import org.example.backend.global.common.constants.BaseResponse;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestPart;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 
+import io.swagger.v3.oas.annotations.Operation;
 import lombok.RequiredArgsConstructor;
 
 @RestController
 @RequiredArgsConstructor
+@RequestMapping("/product-boards")
 public class BoardController {
 	private final ProductBoardService productBoardService;
+	private final Integer SIZE = 10;
 
-	@RequestMapping(value = "/product-boards", method = RequestMethod.POST, consumes = {MediaType.APPLICATION_JSON_VALUE , MediaType.MULTIPART_FORM_DATA_VALUE})
+	@PostMapping(consumes = {MediaType.APPLICATION_JSON_VALUE , MediaType.MULTIPART_FORM_DATA_VALUE})
 	public BaseResponse create(@RequestPart("boardCreateRequest") BoardDto.BoardCreateRequest boardCreateRequest,
 		@RequestPart("productThumbnails") MultipartFile[] productThumbnails,
 		@RequestPart("productDetail") MultipartFile productDetail) {
 		productBoardService.create(boardCreateRequest, productThumbnails, productDetail);
 		return new BaseResponse();
+	}
+
+	@Operation(summary = "판매자 회원 게시글 조회 API")
+	@GetMapping(value = "/company/list")
+	public BaseResponse list(Integer page) {
+		Pageable pageable = PageRequest.of(page, SIZE, Sort.Direction.DESC, "idx");
+		Page<BoardDto.BoardListResponse> boardListResponses = productBoardService.list(pageable);
+		return new BaseResponse(boardListResponses);
 	}
 }
--- backend/src/main/java/org/example/backend/domain/board/repository/ProductBoardRepository.java
+++ backend/src/main/java/org/example/backend/domain/board/repository/ProductBoardRepository.java
@@ -1,7 +1,12 @@
 package org.example.backend.domain.board.repository;
 
 import org.example.backend.domain.board.model.entity.ProductBoard;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 public interface ProductBoardRepository extends JpaRepository<ProductBoard, Long> {
+	@Query("SELECT pb FROM ProductBoard pb JOIN FETCH pb.category")
+	Page<ProductBoard> findAllWithCategory(Pageable pageable);
 }
