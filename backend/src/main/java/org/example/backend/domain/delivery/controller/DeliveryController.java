a7a990472c5bfcd7d2edcff34fe533789972d246
kangkigs <dlflq11@gmail.com>
2024-09-19 08:03:00 +0900

    [Feat] 배송지 목록조회 기능 구현 (#14)

--- backend/src/main/java/org/example/backend/domain/delivery/controller/DeliveryController.java
+++ backend/src/main/java/org/example/backend/domain/delivery/controller/DeliveryController.java
@@ -1,14 +1,29 @@
 package org.example.backend.domain.delivery.controller;
 
 import lombok.RequiredArgsConstructor;
+import org.example.backend.domain.delivery.model.dto.DeliveryDto;
 import org.example.backend.domain.delivery.service.DeliveryService;
+import org.example.backend.global.common.constants.BaseResponse;
+import org.example.backend.global.security.custom.model.dto.CustomUserDetails;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/delivery")
 @RequiredArgsConstructor
 public class DeliveryController {
     private final DeliveryService deliveryService;
+
+    @GetMapping("/list")
+    public BaseResponse getList(
+            @AuthenticationPrincipal CustomUserDetails userDetails
+            ){
+        List<DeliveryDto.DeliveryResponse> deliveryResponses = deliveryService.getList(userDetails.getIdx());
+        return new BaseResponse(deliveryResponses);
+    }
 
 }
--- backend/src/main/java/org/example/backend/domain/delivery/service/DeliveryService.java
+++ backend/src/main/java/org/example/backend/domain/delivery/service/DeliveryService.java
@@ -1,8 +1,13 @@
 package org.example.backend.domain.delivery.service;
 
 import lombok.RequiredArgsConstructor;
+import org.example.backend.domain.delivery.model.dto.DeliveryDto;
+import org.example.backend.domain.delivery.model.entity.Delivery;
 import org.example.backend.domain.delivery.repository.DeliveryRepository;
+import org.example.backend.global.common.constants.BaseResponseStatus;
+import org.example.backend.global.exception.InvalidCustomException;
 import org.springframework.stereotype.Service;
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
@@ -10,4 +15,11 @@ public class DeliveryService {
     private final DeliveryRepository deliveryRepository;
 
 
+    public List<DeliveryDto.DeliveryResponse> getList(Long idx) {
+        List<Delivery> deliveries = deliveryRepository.findAllByUserIdx(idx).orElseThrow(
+                () -> new InvalidCustomException(BaseResponseStatus.USER_DELIVERY_LIST_FAIL)
+        );
+
+        return deliveries.stream().map(Delivery::toDeliveryResponse).toList();
+    }
 }
