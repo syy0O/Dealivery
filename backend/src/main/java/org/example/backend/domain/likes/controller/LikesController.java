23d1f11a00889a0810a9a68226f3aac0b3b09598
kangkigs <dlflq11@gmail.com>
2024-09-23 05:18:03 +0900

    [Feat] 관심 등록/취소 기능 구현 (#39)

--- backend/src/main/java/org/example/backend/domain/likes/controller/LikesController.java
+++ backend/src/main/java/org/example/backend/domain/likes/controller/LikesController.java
@@ -1,9 +1,28 @@
 package org.example.backend.domain.likes.controller;
 
+import lombok.RequiredArgsConstructor;
+import org.example.backend.domain.likes.model.dto.LikesDto;
+import org.example.backend.domain.likes.service.LikesService;
+import org.example.backend.global.common.constants.BaseResponse;
+import org.example.backend.global.security.custom.model.dto.CustomUserDetails;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/likes")
+@RequiredArgsConstructor
 public class LikesController {
+    private final LikesService likesService;
+
+    @PostMapping("")
+    public BaseResponse like(
+            @RequestBody LikesDto.LikeRequest request,
+            @AuthenticationPrincipal CustomUserDetails userDetails
+            ){
+        likesService.like(request, userDetails.getIdx());
+        return new BaseResponse();
+    }
 }
--- backend/src/main/java/org/example/backend/domain/likes/repository/LikesRepository.java
+++ backend/src/main/java/org/example/backend/domain/likes/repository/LikesRepository.java
@@ -4,6 +4,9 @@ import org.example.backend.domain.likes.model.entity.Likes;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface LikesRepository extends JpaRepository<Likes, Long> {
+    Optional<Likes> findByProductBoardIdxAndUserIdx(Long productBoardIdx, Long userIdx);
 }
