76519c61d9c0b819e130a7511f6f74555d3fc871
gyulim5 <ohrbfla1004@khu.ac.kr>
2024-10-01 09:51:07 +0900

    [Docs] nginx 설정 파일 변경

--- frontend/src/components/company/CompanyBoardPostComponent.vue
+++ frontend/src/components/company/CompanyBoardPostComponent.vue
@@ -51,7 +51,7 @@
                         :disabled="!isActivate"
                       />
                     </span>
-                    <p class="char-count">{{ charCount }} / 18</p>
+                    <p class="char-count">{{ charCount }} / 50</p>
                   </div>
                 </td>
               </tr>
@@ -328,10 +328,10 @@ export default {
     },
     validateTitle() {
       this.charCount = this.title.length;
-      if (this.charCount > 18) {
-        alert("게시글 제목은 18자 이하로 입력해야 합니다.");
-        this.title = this.title.slice(0, 18);
-        this.charCount = 18;
+      if (this.charCount > 50) {
+        alert("게시글 제목은 50자 이하로 입력해야 합니다.");
+        this.title = this.title.slice(0, 50);
+        this.charCount = 50;
         return;
       }
     },
@@ -364,11 +364,11 @@ export default {
         alert(this.dateErrorMsg);
         return false;
       }
-      if (this.title.length < 1 || this.title.length > 18) {
+      if (this.title.length < 1 || this.title.length > 50) {
         alert(
           this.title.length === 0
             ? "제목을 입력해주세요."
-            : "제목을 18자 이하로 입력해주세요."
+            : "제목을 50자 이하로 입력해주세요."
         );
         return false;
       }
