ebe3b8ce82d024f12743812345dc0d7074d07cf1
gyulim5 <ohrbfla1004@khu.ac.kr>
2024-10-07 17:37:14 +0900

    [Init] 대기열 프로젝트 추가

---
+++ queue-system/quequeflow/src/main/java/com/example/quequeflow/QuequeflowApplication.java
@@ -0,0 +1,13 @@
+package com.example.quequeflow;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuequeflowApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QuequeflowApplication.class, args);
+	}
+
+}
