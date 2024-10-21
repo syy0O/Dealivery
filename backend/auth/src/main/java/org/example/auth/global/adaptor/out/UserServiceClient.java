f2ee62f5c9bb081dcc058b7f2f66e07c1bc40c78
kangkigs <dlflq11@gmail.com>
2024-10-22 00:23:27 +0900

    [Hotfix] openFeign설정 수정

--- backend/auth/src/main/java/org/example/auth/global/adaptor/out/UserServiceClient.java
+++ backend/auth/src/main/java/org/example/auth/global/adaptor/out/UserServiceClient.java
@@ -2,10 +2,12 @@ package org.example.auth.global.adaptor.out;
 
 import org.example.auth.domain.user.model.entity.User;
 import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.stereotype.Component;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 @FeignClient(name = "user-service", url = "${custom.feign-url}", contextId = "userServiceClientForAuth")
+@Component
 public interface UserServiceClient {
 
     @GetMapping("/user/user-email")
