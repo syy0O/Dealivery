b7a921a932bb3c615e73b206896a7efd8a26f049
syy0O <songie05@naver.com>
2024-10-23 21:23:31 +0900

    [Fix] 락 유지시간 변경

--- queue-system/quequeflow/src/main/java/com/example/quequeflow/global/aop/DistributedLock.java
+++ queue-system/quequeflow/src/main/java/com/example/quequeflow/global/aop/DistributedLock.java
@@ -18,11 +18,11 @@ public @interface DistributedLock {
      * 락을 기다리는 시간 (default - 5s)
      * 락 획득을 위해 waitTime 만큼 대기한다
      */
-    long waitTime() default 20L;
+    long waitTime() default 5L;
 
     /**
      * 락 임대 시간 (default - 3s)
      * 락을 획득한 이후 leaseTime 이 지나면 락을 해제한다
      */
-    long leaseTime() default 5L;
+    long leaseTime() default 3L;
 }
